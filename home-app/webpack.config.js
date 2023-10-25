49a022dcc268e99f15efc4af40f344c972bbf65f
Agung Laksmana <agunglaksmana908@gmail.com>
2023-10-25 22:12:34 +0700

    :sparkles: simple concept micro frontend with webpack module federation

---
+++ home-app/webpack.config.js
@@ -0,0 +1,63 @@
+//home-app/webpack.config.js
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+// import dependencies from package.json, which includes react and react-dom
+const { dependencies } = require("./package.json");
+
+module.exports = {
+  entry: "./src/entry.js",
+  mode: "development",
+  devServer: {
+    port: 3000, // port 3001 for header-app
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "HomeApp",
+      remotes: {
+        HeaderApp: "HeaderApp@http://localhost:3001/remoteEntry.js",
+      },
+      shared: {
+        // and shared
+        ...dependencies, // other dependencies
+        react: {
+          // react
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          // react-dom
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+};
